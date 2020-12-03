7efa9491ae26a842a69a71d28e47fb1834844829
chrisfollen <christopher.follen@gmail.com>
2020-12-03 11:00:11 -0700

    Stoic message program outline

---
+++ stoic_message.js
@@ -0,0 +1,29 @@
+
+
+//create function to generate a random number
+
+
+
+//create object containing arrays of message parts
+
+
+
+//create array for stoic messages
+
+
+//iterate through message types, calling random number function to select one of each message type
+//store messages to be used in 'wisdom' array
+
+
+
+
+
+
+
+
+
+
+
+//join messages as string + display final string
+
+
